0ed5956ac3d96e466aa73c42c1ad7935bead9d69
jahruhn <48630231+jahruhn@users.noreply.github.com>
2022-07-06 16:59:42 +0200

    Add files via upload

---
+++ smws.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Which website would you like to scrape?
+
+read varname
+
+echo I shall now scrape $varname
+
+lynx -accept_all_cookies -dump $varname | awk '/linkedin/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/twitter/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/youtube/{print $2}' >> $varname.socialmedialinks.txt
