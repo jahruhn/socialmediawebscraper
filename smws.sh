fcb8a69202f6cb57fd13727264f1e911a65de9b2
jahruhn <48630231+jahruhn@users.noreply.github.com>
2023-05-03 14:31:21 +0200

    Update smws.sh
    added the Mastodon social network

--- smws.sh
+++ smws.sh
@@ -6,4 +6,4 @@ read varname
 
 echo I shall now scrape $varname
 
-lynx -accept_all_cookies -dump $varname | awk '/linkedin/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/twitter/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/youtube/{print $2}' >> $varname.socialmedialinks.txt
+lynx -accept_all_cookies -dump $varname | awk '/linkedin/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/twitter/{print $2}' >> $varname.socialmedialinks.txt && lynx -accept_all_cookies -dump $varname | awk '/youtube/{print $2}' >> $varname.socialmedialinks.txt lynx -accept_all_cookies -dump $varname | awk '/mastodon/{print $2}' >> $varname.socialmedialinks.txt
